29b81de693b6a8095851f5050311dbb036622a36
Jigar <jigaragrawal29@gmail.com>
2018-09-22 21:24:23 -0700

    some structural changes

--- main_script.m
+++ main_script.m
@@ -1,4 +1,11 @@
+%Clearing the environment 
+clc; 
+clear all; 
 
+%Adding search paths to the directory 
+addpath('functions', 'dataset'); 
+
+%loading the dataset 
 load mnist_data20by20.mat
 
 disp(getSigmoidArray([1, 3, 4]))
