94954fd836fe8c0a7ef752bdaa7464892dcad564
vidhanmodi95 <vidhanmodi95@gmail.com>
2018-11-14 19:23:37 -0800

    Debug weight creation function added

---
+++ functions/initializeDebugWeights.m
@@ -0,0 +1,18 @@
+%Function       : initializeDebugWeights
+%Arguments      : num_input  - number of inputs in the layer  
+%                 num_outputs - number of outputs to the layer 
+%Description    : This function creates the weights for debugging the
+%                 trained neural network  
+%Returns        : Debug weights  
+function output_weights = initializeDebugWeights(num_inputs, num_outputs)
+
+%Creating the array of zeros of the given size 
+output_weights = zeros(num_outputs, num_inputs+1);
+
+%Creating the sinosodual values for the weights to debug the neural network
+temp_weights = sin(1:numel(output_weights)); 
+
+%Reshaping the linear array into the given shape of the layer 
+output_weights = reshape(temp_weights, size(output_weights)) / 10;
+end
+
